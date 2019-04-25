7111fe1dae86da66caaf6ecd3b17d3582b164ec0
Martin <martin@seventy2eleven.de>
2019-04-25 15:30:48 +0200

    Makes rotation working.

--- project/scripts/table.gd
+++ project/scripts/table.gd
@@ -1,18 +1,23 @@
 extends Node2D
 
 var ball_scene = preload("res://scenes/ball.tscn") # Will load when parsing the script.
+var white_ball_origin : Vector2 = Vector2()
+var white_ball : RigidBody2D
+
+func _ready():
+	white_ball = find_node("white-ball")
+	white_ball_origin = white_ball.position
 
 func _input(event):
 	if event is InputEventMouseButton:
 			print("Applying force...")
-			var ball : RigidBody2D = find_node("white-ball")
 			var direction = Vector2(-1,0);
-			var impulse = direction * 100;
-			ball.apply_impulse(Vector2(),impulse)
+			var impulse = direction * 700;
+			white_ball.apply_impulse(Vector2(),impulse)
 
 func _on_game_reset_sig():
-	var ball = find_node("white-ball")
-	ball.queue_free()
-	remove_child(ball)
-	add_child(ball_scene.instance())
-#	var newBall = instance
+	white_ball.queue_free()
+	remove_child(white_ball)
+	white_ball = ball_scene.instance()
+	white_ball.position = white_ball_origin
+	add_child(white_ball)
